3d7902b8a4736afcad27700c10a4494e33fe605f
Matheus Maria <matheus.maria@oransys.net>
2019-11-25 15:05:10 -0300

    First Commit

---
+++ Projects/contador_de_pessoas/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Contador de Pessoas",
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Pessoas: 0",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text(
+                "-1",
+                style: TextStyle(fontSize: 40, color: Colors.white),
+              ),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text(
+                "+1",
+                style: TextStyle(fontSize: 40, color: Colors.white),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        Text(
+          "Pode Entrar!",
+          style: TextStyle(
+              color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+        ),
+      ],
+    ),
+    debugShowCheckedModeBanner: false,
+  ));
+}
---
+++ Projects/first_app/lib/main.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+
+import 'package:first_app/models/item.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Primeiro App do Matheus',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    // items.add(Item(title: "Item 1", done: false));
+    // items.add(Item(title: "Item 2", done: true));
+    // items.add(Item(title: "Item 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isEmpty) return;
+
+    setState(() {
+      widget.items.add(Item(title: newTaskCtrl.text, done: false));
+      newTaskCtrl.text = "";
+    });
+
+    save();
+  }
+
+  void remove(int index){
+    setState(() {
+      widget.items.removeAt(index);
+    });
+    save();
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if(data != null){
+      Iterable decoded = jsonDecode(data);
+
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+
+      setState(() {
+        widget.items = result;
+      });
+
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+_HomePageState(){
+  load();
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration.collapsed(
+            hintText: "Nova Tarefa",
+            hintStyle: TextStyle(color: Colors.white)
+          )
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = widget.items[index];
+
+          return Dismissible(
+            key: Key(item.title),
+            background: Container(
+              color: Colors.red.withOpacity(0.8),
+            ),
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                });              
+                save();
+              },
+            ),
+            onDismissed: (direction){
+              remove(index);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
+    );
+  }
+}
